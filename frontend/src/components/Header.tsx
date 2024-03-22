0d2d73e19bacf083f1642841f3856674a57f58b5
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2024-03-22 19:03:35 +0200

    mob burger

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -7,6 +8,9 @@ const Header = () => {
             <Link to="/" className="text-2xl font-bold tracking-tight text-green-500">
                 SharePlate.com
             </Link>
+            <div className="md:hidden">
+                <MobileNav/>
+            </div>
         </div>
     </div>
   )
