d54eee7c2c23578aea19dab63c5ca3fc6d92a392
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2024-03-27 20:28:02 +0200

    user profile

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,11 +1,15 @@
-import { userUpdateMyUser } from "@/api/MyUserApi";
+import { useGetMyUser, userUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
+  const { currentUser, isLoading: isGetLoading } = useGetMyUser();
+  const { updateUser, isLoading: isUpdateLoading } = userUpdateMyUser();
 
-  const { updateUser, isLoading} = userUpdateMyUser();
+  if (isGetLoading) {
+    return <span>Loading...</span>;
+  }
   
-  return <UserProfileForm onSave={updateUser} isLoading={isLoading}/>;
+  return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading}/>;
 };
 
 export default UserProfilePage;
