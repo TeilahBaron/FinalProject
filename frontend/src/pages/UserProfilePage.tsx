ed901a6506ff5495b93756b2901b2e176b5cf291
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2024-03-29 01:05:14 +0200

    commit

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -8,8 +8,15 @@ const UserProfilePage = () => {
   if (isGetLoading) {
     return <span>Loading...</span>;
   }
+
+  if(!currentUser) {
+    return <span>Unable to load user profile </span>;
+  }
   
-  return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading}/>;
+  return <UserProfileForm 
+   currentUser={currentUser}
+   onSave={updateUser} 
+   isLoading={isUpdateLoading}/>;
 };
 
 export default UserProfilePage;
