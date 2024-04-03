0ae50a4cba9e87af8b9218118e754a9986e4f968
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2024-04-04 00:49:22 +0300

    desktop nav

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+  return(
+    <Button 
+    variant="ghost"
+    className="font-bold hover:text-cyan-600 hover:bg-white">
+    Log In
+    </Button>
+  )
+}
+
+export default MainNav;
