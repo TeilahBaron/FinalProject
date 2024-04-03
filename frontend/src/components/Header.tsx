5d9fdc0d543daa0571f4c014c618642abd1ddc37
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2024-04-04 00:42:06 +0300

    mob nav

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return(
-    <div className="border-b-2 border-b-cyan-600 py-6">
-        <div className="container mx-auto flex justify-between items-center">
-            <Link to="/" 
-            className="text-2xl font-bold tracking-tight text-cyan-600">
-                SharePlate
-            </Link>
-        </div>
+  <div className="border-b-2 border-b-cyan-600 py-6">
+    <div className="container mx-auto flex justify-between items-center">
+      <Link to="/" 
+      className="text-2xl font-bold tracking-tight text-cyan-600">
+      SharePlate
+      </Link>
+      <div className="md:hidden">
+        <MobileNav/>
+      </div>
     </div>
+  </div>
   )
 }
 
