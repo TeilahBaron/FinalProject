c8c7b2c01c0b3d2aae58e03b16d9f8c96004bd82
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2024-04-07 20:57:57 +0300

    user profile form

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,6 +1,20 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
+const getCurrentUser = async (req: Request, res: Response) =>{
+  try {
+    const currentUser = await User.findOne({ _id: req.userId });
+    if (!currentUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      res.json(currentUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
 const createCurrentUser = async (req: Request, res: Response) =>{
 try {
     const { auth0Id } = req.body; 
@@ -39,6 +53,8 @@ try {
 }
 };
 
-export default{createCurrentUser,
+export default{
+    createCurrentUser,
     updateCurrentUser,
+    getCurrentUser,
 };
