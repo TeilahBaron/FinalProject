28acb06a49f6d4834fc8b9fb93eca528ba4621aa
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2024-04-11 20:27:22 +0300

    route

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -7,6 +7,12 @@ const router = express.Router();
 
 router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
-router.put("/", jwtCheck, jwtParse, validateMyUserRequest,MyUserController.updateCurrentUser);
+router.put(
+    "/",
+    jwtCheck,
+    jwtParse,
+    validateMyUserRequest,
+    MyUserController.updateCurrentUser
+  );
 
 export default router;
