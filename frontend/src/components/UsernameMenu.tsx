274e339ed1decff2f01ca3704b5e55a13f15cde4
TeilahBaron <139397326+TeilahBaron@users.noreply.github.com>
2024-04-15 22:34:59 +0300

    restaurant form frontend

--- frontend/src/components/UsernameMenu.tsx
+++ frontend/src/components/UsernameMenu.tsx
@@ -14,6 +14,11 @@ const UsernameMenu = () => {
        {user?.email}
        </DropdownMenuTrigger>
        <DropdownMenuContent>
+       <DropdownMenuItem>
+        <Link to="/manage-restaurant" className="font-bold hover:text-cyan-600">
+            Manage Restaurant
+        </Link>
+        </DropdownMenuItem>
         <DropdownMenuItem>
         <Link to="/user-profile" className="font-bold hover:text-cyan-600">
             User Profile
